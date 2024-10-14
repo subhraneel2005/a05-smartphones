458aa34f40d420e6f6d768379548e687a2e45ac6
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-10-14 19:18:33 +0530

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='min-h-screen w-full bg-zinc-900 text-zinc-300 flex justify-center items-center'>
+      <h1 className='text-[5rem] font-bold'>Hello, world!</h1>
+    </div>
+  )
+}
