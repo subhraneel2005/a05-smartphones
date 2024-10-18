0b73fe68fbbcde5dea57be45a77d813ef186a559
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-10-19 00:44:31 +0530

    2 frames done

--- src/components/frames/Frame1.tsx
+++ src/components/frames/Frame1.tsx
@@ -26,7 +26,7 @@ export default function Frame1() {
   return (
     <div className="md:rounded-[36px] rounded-[16px] bg-[url('/bgTrees.png')] bg-cover bg-center">
             {/* Centered treesMobile image */}
-            <div className="md:px-[50px] lg:px-[208px] md:pt-[20px] lg:pt-[80px] px-[14px] pt-[9.2px]">
+            <div className="md:px-[50px] xl:px-[208px] md:pt-[20px] xl:pt-[80px] px-[14px] pt-[9.2px]">
                 <img
                     src="/treesMobile.png"
                     alt="Trees"
@@ -35,14 +35,14 @@ export default function Frame1() {
             </div>
     
             {/* Divs containing texts */}
-            <div className="grid grid-cols-2 md:flex md:justify-center md:items-center px-[16px] pb-[24.13px] pt-[24px] gap-[16px] md:gap-[24px] lg:px-[169px] lg:pb-[51px] lg:pt-[39px] lg:gap[42px]">
+            <div className="grid grid-cols-2 md:flex md:justify-center md:items-center px-[16px] pb-[24.13px] pt-[24px] gap-[16px] md:gap-[24px] xl:px-[169px] xl:pb-[51px] xl:pt-[39px] xl:gap[42px]">
                 {data.map((d) => (
                     <div
                     key={d.id}
                     className="flex flex-col items-start"
                 >
-                    <p className="text-[#fff] md:text-desktop/h4 lg:text-desktop/h3 text-mobile/h6 opacity-[80%]">{d.name}</p>
-                    <p className="text-[#fff] text-mobile/small/body md:text-desktop/body/2/regular lg:text-desktop/body/large text-opacity-70 md:pt-0 lg:pt-[8px]">{d.description}</p>
+                    <p className="text-[#fff] md:text-desktop/h4 xl:text-desktop/h3 text-mobile/h6 opacity-[80%]">{d.name}</p>
+                    <p className="text-[#fff] text-mobile/small/body md:text-desktop/body/2/regular xl:text-desktop/body/large text-opacity-70 md:pt-0 xl:pt-[8px]">{d.description}</p>
                 </div>
                 ))}
             </div>
--- src/components/frames/Frame2.tsx
+++ src/components/frames/Frame2.tsx
@@ -2,24 +2,24 @@
 
 export default function Frame2() {
   return (
-    <div className="relative mt-[16px] bg-[#FCCE8D] bg-opacity-[20%] border border-[#FCCE8D] w-full h-full min-h-[400px] md:h-[739px] flex flex-col items-center md:rounded-[36px] rounded-[16px]">
+    <div className="relative mt-[16px] bg-[#FCCE8D] bg-opacity-[20%] border border-[#FCCE8D] w-full h-full min-h-[400px] md:h-[739px] flex flex-col items-center md:rounded-[36px] rounded-[16px] overflow-hidden">
     {/* text div */}
-    <div className="flex flex-col justify-start items-center z-10 md:px-[244.5px] md:pt-[34px] px-[12px] pt-[16px]">
-        <h2 className="md:text-desktop/h2 text-mobile/h4 text-center max-w-full">App Harmony: Speed Performance</h2>
+    <div className="flex flex-col justify-start items-center z-10 xl:px-[244.5px] xl:pt-[34px] px-[12px] pt-[16px] md:px-[20px] md:pt-[24px]">
+        <h2 className="xl:text-desktop/h2 md:text-desktop/h3 text-mobile/h4 text-center max-w-full">App Harmony: Speed Performance</h2>
         <span className="md:py-[20px]">
             <h4 className="md:text-desktop/h4 text-mobile/h6 text-[#232323] text-opacity-85 text-center max-w-full py-[12px]">8GB (*4GB+4GB*) RAM + 64GB ROM</h4>
         </span>
-        <p className="md:text-desktop/body/large text-desktop/body/2/regular text-[#1C1B1B] text-opacity-70 max-w-3xl text-center">
+        <p className="xl:text-desktop/body/large md:text-desktop/body/2/medium text-desktop/body/2/regular text-[#1C1B1B] text-opacity-70 max-w-3xl text-center">
         Juggling apps has never been easier! Glide through your tasks with a fluid experience that makes multitasking a breeze.
         </p>
     </div>
     
     {/* img div */}
-    <div className="md:px-[101px] md:pt-[117px] md:pb-[52px] absolute z-0 md:flex hidden">
+    <div className="xl:px-[101px] xl:pt-[170px] xl:pb-[52px] md:px-[12px] md:pb-[64px] md:pt-[160px] absolute z-0 md:flex hidden">
         <img
         src="/orange.png"
         alt="Trees"
-        className="w-[1045px] h-full mx-auto"
+        className="w-[1000px] h-full mx-auto"
         />
     </div>
     <div className="px-[12px] pb-[64px] pt-[160px] absolute z-0 md:hidden flex">
